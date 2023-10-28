e3a165428907dbbe218e860bf4350e49ef6e3379
arifrain26 <arifrain26@gmail.com>
2023-10-28 12:55:38 +0530

    update api/index

--- api/index.js
+++ api/index.js
@@ -1,40 +1,34 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
-import cookieParser from "cookie-parser";
-import path from "path";
-
-//routes
 import userRouter from "./routes/user.route.js";
 import authRouter from "./routes/auth.route.js";
 import listingRouter from "./routes/listing.route.js";
-
+import cookieParser from "cookie-parser";
+import path from "path";
 dotenv.config();
-const app = express();
-app.use(express.json());
-app.use(cookieParser());
 
 mongoose
-  .connect(process.env.MONGO, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    family: 4,
-  })
+  .connect(process.env.MONGO)
   .then(() => {
-    console.log("connected to mongodb");
+    console.log("Connected to MongoDB!");
+  })
+  .catch((err) => {
+    console.log(err);
   });
 
 const __dirname = path.resolve();
 
+const app = express();
+
+app.use(express.json());
+
+app.use(cookieParser());
+
 app.listen(3000, () => {
-  console.log("server is running on port 3000");
+  console.log("Server is running on port 3000!");
 });
 
-app.get("/", (req, res) => {
-  res.json({
-    message: "Hello worlds",
-  });
-});
 app.use("/api/user", userRouter);
 app.use("/api/auth", authRouter);
 app.use("/api/listing", listingRouter);
@@ -47,7 +41,7 @@ app.get("*", (req, res) => {
 
 app.use((err, req, res, next) => {
   const statusCode = err.statusCode || 500;
-  const message = err.message || "Internal server error";
+  const message = err.message || "Internal Server Error";
   return res.status(statusCode).json({
     success: false,
     statusCode,
